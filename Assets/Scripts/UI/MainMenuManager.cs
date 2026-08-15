bd4149fd25a37ccbff8020c425a2dcdf7dc94e78
Radostin Radev <radostin.radev@iodigital.com>
2026-08-15 17:58:54 +0300

    Information panel added

---
+++ Assets/Scripts/UI/MainMenuManager.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Assets.Scripts.UI
+{
+    public class MainMenuManager : MonoBehaviour
+    {
+        [SerializeField] private GameObject mainMenuCanvas;
+        [SerializeField] private Button informationButton;
+        
+        [SerializeField] private GameObject informationCanvas;
+        [SerializeField] private Button closeInformationButton;
+
+        private void Awake()
+        {
+            if(informationButton != null) informationButton.onClick.AddListener(ShowInformation);
+            if(closeInformationButton != null) closeInformationButton.onClick.AddListener(ShowMainMenu);
+        }
+
+        private void OnDestroy()
+        {
+            if (informationButton != null) informationButton.onClick.RemoveListener(ShowInformation);
+            if (closeInformationButton != null) closeInformationButton.onClick.RemoveListener(ShowMainMenu);
+        }
+
+        private void ShowMainMenu() => SetCanvasActive(mainMenuCanvas);
+        private void ShowInformation() => SetCanvasActive(informationCanvas);
+
+        private void SetCanvasActive(GameObject canvas)
+        {
+            mainMenuCanvas.SetActive(mainMenuCanvas == canvas);
+            informationCanvas.SetActive(informationCanvas == canvas);
+        }
+    }
+}
